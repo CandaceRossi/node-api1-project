a44fb41d4ba7241399b8ca2d354f3f1d5ae8f8b5
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-10-30 18:24:18 -0400

    finished crud operations in request handler

--- index.js
+++ index.js
@@ -1,103 +1,127 @@
-// implement your API here
-const express = require("express");
+const express = require("express"); // import the express package
+
+const server = express(); // creates the server
 
 const db = require("./data/db.js");
 
-const server = express();
-
-//middleware
-server.use(expressjson());
-
-//request/route handlers
-
-//POST
-
-//If req body is missing the name or bio property
-server.post('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const name = req.params.id;
-
-    db.insert(id, name).then(user => {
-        res.status(400).json({errorMessage: "Please provide name and bio for user."});
-  })
-    .catch(err => {
-        console.log("error", err);
-        res.status(500).json({error: "failed to add the user to the db"})
-    })
-})
-//If the information about the user is valid
-server.post('/users', (req, res) => {
-    const userInformation = req.body;
-
-    db.insert(userInformation).then(user => {
-        res.status(201).json(user);
-    })
-    .catch(err => {
-        console.log("error", err);
-        res.status(500).json({error: "failed to add the user to the database"})
-    })
-})
-
-//If there's an error while saving the user
-server.post('/users', (req, res) => {
-
-db.cancel(req).then(res => {
-        res.status(500).json({error: "There was an error while saving the user to the database "});
-    })
-.catch(err => {
-        console.log("error", err);
-        res.status(500).json({error: "failed to add the user to the database"})
-    })
-})
-
-//GET 
+//middleware;
+server.use(express.json());
 
 //request to / on localhost:8000
 server.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).send("Hello Magi's World");
 });
 
-//request /api/users that returns a list of Users
-server.get('/api/users', (req, res) => {
-    db.find(req).then(users => {
-        res.status(200).json(users);
-    }).catch(err => {
-        console.log("error", err);
-        res.status(500).json({error: "failed to get users from db"})
-    })
-})
-
 //listen for request in port on localhost
-server.status(200).listen(8000, () => console.log("API running on port 8000"));
+server.listen(8000, () =>
+  console.log("Server running on http://localhost:8000")
+);
 
-//PUT 
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The users information cannot be retrieved." });
+    });
+});
+
+//POST
+//If req body is missing the name or bio property
+//If the information about the user is valid
+//If there's an error while saving the user
+
+server.post("/api/users", (req, res) => {
+  const userInformation = req.body;
+  db.insert(userInformation)
+    .then(id => {
+      if (id) {
+        res.status(201).json(id);
+      } else {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for user." });
+      }
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ error: "failed to add the user to the db" });
+    });
+});
+
+// DELETE
+// if user with the specified id is not found
+// if there is an error in removing the user from the database
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(res => {
+      if (id) {
+        res.status(200).json({
+          message: `The user with the specified ID ${id} has been remove`
+        });
+      } else {
+        res.status(404).json({
+          error: `The user with the specified ID ${id} does not exist.`
+        });
+      }
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ error: "INTERNAL ERROR" });
+    });
+});
+
+// GET
+// if user with specific id is not found:
+// if there is an error in retrieving the user from database
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(users => {
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res
+          .status(500)
+          .json({ error: "The user information could not be retrieved." });
+      }
+    })
+    .catch(err => {
+      res.status(404).json({
+        message: `The user with the specified ID ${id} does not exist.`
+      });
+    });
+});
+
+// //PUT
+// if user with specific id is not found
+// if request body is missing the name or bio property
+// if there is an error when updating the user
+// if the user is found and the new information is valid
+
 server.put("/api/users/:id", (req, res) => {
-    const changes = req.body;
-})
-Users.update(req.params.id, changes) .then(user => {
-    if (user) {
-        res.status(200).send(user);
-    } else {
-        res.status(404).send({message: "The user with the specified ID does not exist."})
-    }
-}).catch (err => console.log(err))
-res.status(500).send({message: "error updating the hub"})
+  const changes = req.body;
+  const id = req.params.id;
 
-
-
-db.find().then(data => {
-    res.send(data);
-})
-
-
-
-//db.insert().then(data => {
-    //res.
-//})
-
-const users = {
-  name: "Jane Doe", // String, required
-  bio: "Not Tarzan's Wife, another Jane",  // String
-  created_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
-  updated_at: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
-}
+  db.update(id, changes)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          message: `The user with the specified ID ${id} does not exist.`
+        });
+      }
+    })
+    .catch(err => console.log(err));
+  res
+    .status(500)
+    .send({ message: "The user information could not be modified." });
+});
